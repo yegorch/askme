0b504b30578dc41f8b9b8213c1d0f6104948f8eb
yegorch <yegor.rb@gmail.com>
2021-04-11 19:00:07 +0600

    clean ask user+

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,58 @@
 require 'openssl'
 
 class User < ActiveRecord::Base
+
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
+  VALIDATE_NAME = /\A\w+\z/
+
   attr_accessor :password
-
+ 
   has_many :questions
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
-  validates :password, presence: true, on: :create
-  validates_confirmation_of :password
 
+  before_validation :downcase_username, :downcase_email
   before_save :encrypt_password
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :username,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 40 },
+    format: { with: VALIDATE_NAME }
+
+  def downcase_username
+    username&.downcase!
+  end
+
+  def downcase_email
+    email&.downcase!
+  end  
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    return nil unless user.present?
+
+    hashed_password = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(
+        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
+      )
+    )
+    return user if user.password_hash == hashed_password
+
+    nil
+  end
+
+  private
 
   def encrypt_password
     if password.present?
@@ -23,25 +64,5 @@ class User < ActiveRecord::Base
         )
       )
     end
-  end
-
-
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
-
-  def self.authenticate(email, password)
-  
-    user = find_by(email: email)
-
-    return nil unless user.present?
-
-    hashed_password = User.hash_to_string(
-      OpenSSL::PKCS5.pbkdf2_hmac(
-        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
-      )
-    )
-    return user if user.password_hash == hashed_password
-    nil
   end
 end
